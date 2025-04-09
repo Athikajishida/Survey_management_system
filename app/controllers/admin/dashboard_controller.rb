d2158e5a15b231839cb059edf9cbb5840324e9f1
Athika Jishida <athikajishida@gmail.com>
2025-04-09 21:04:43 +0530

    Dssh board with cards

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < Admin::BaseController
   def index
+    @surveys = Survey.all.order(created_at: :desc)
+    @survey = Survey.find_by(id: params[:survey_id]) || @surveys.first
+
+    if @survey
+      @avg_scores = KpiCalculator.average_score_per_category(@survey.id)
+      @engagement_index = KpiCalculator.employee_engagement_index(@survey.id)
+      @performance_brackets = KpiCalculator.performance_brackets(@survey.id)
+      @category_comparison = KpiCalculator.category_performance_comparison(@survey.id)
+      @dynamic_thresholds = KpiCalculator.dynamic_classification_thresholds(@survey.id)
+    end
   end
 end
